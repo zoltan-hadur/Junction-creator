02b96ac5098c6d4afb0af24a65cf29a271cadbd7
Hadur Zoltán <zotya701@hotmail.hu>
2017-04-30 06:55:22 +0200

    Removed unused directives

--- Junction creator/Form1.cs
+++ Junction creator/Form1.cs
@@ -1,16 +1,7 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
 using NCode.ReparsePoints;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Junction_creator
